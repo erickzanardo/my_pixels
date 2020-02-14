5603514e0fb643eb7d6c9a91d79ae8ec8763083a
Thomas Horta <thomas.horta@dextra-sw.com>
2020-02-14 19:16:23 -0300

    Create Comment model and add fake comments to PixelService
    Signed-off-by: Thomas Horta <thomas.horta@dextra-sw.com>

---
+++ lib/models/comment.dart
@@ -0,0 +1,17 @@
+class Comment {
+  final String pixelId;
+  final String slug;
+  final DateTime posted;
+  final CommentAuthor author;
+  final String text;
+
+  Comment({ this.pixelId, this.slug, this.posted, this.author, this.text });
+}
+
+class CommentAuthor {
+  final String id;
+  final String name;
+  final String picture;
+
+  CommentAuthor({ this.id, this.name, this.picture });
+}
---
+++ lib/services/pixel_service.dart
@@ -0,0 +1,43 @@
+import 'package:my_pixels/models/comment.dart';
+
+import '../models/pixel.dart';
+
+class PixelService {
+  static Future<PixelModel> fetchPixel() {
+    return Future.value(
+      PixelModel(
+        url: 'https://cptblackpixel.dev/assets/projects/relic_raider.png',
+        comments: [
+          Comment(
+            pixelId: "ab42cd",
+            posted: DateTime.parse("2020-02-01 12:15:00"),
+            author: CommentAuthor(
+              id: "thomashorta",
+              name: "Thomas",
+            ),
+            text: "This pixel is awesome!",
+          ),
+          Comment(
+            pixelId: "ab42cd",
+            posted: DateTime.parse("2020-02-07 09:10:00"),
+            author: CommentAuthor(
+              id: "xmenho",
+              name: "Rafael",
+            ),
+            text: "That's cool...",
+          ),
+          Comment(
+            pixelId: "ab42cd",
+            posted: DateTime.parse("2020-02-14 09:10:00"),
+            author: CommentAuthor(
+              id: "erickz",
+              name: "Erick",
+            ),
+            text: "What an amazing landscape, never seen something like this!!!",
+          ),
+
+        ],
+      ),
+    );
+  }
+}
