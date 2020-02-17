204643e006ffcd2fb38a69ed9f297fb011e15ffc
Thomas Horta <thomashorta@gmail.com>
2020-02-17 09:06:25 -0300

    Change color variables to getters
    Signed-off-by: Thomas Horta <thomashorta@gmail.com>

--- lib/common/colors.dart
+++ lib/common/colors.dart
@@ -13,17 +13,17 @@ class Colors {
   static const Color _pixelGreen = Color(0xFF83E0BA);
 
   /// define variables to point to custom colors for specific uses based on
-  /// theme selection (Dark or Light)
+  /// theme selection (Dark or Light) (TODO)
   ///
   /// ex:
-  /// static const Color text = isDarkTheme ? _solidWhite : _solidBlack
-  static const Color detailBackground = _solidWhite;
+  /// static Color get text => isDarkTheme ? _solidWhite : _solidBlack
+  static Color get detailBackground => _solidWhite;
 
-  static const Color placeHolderColor = _pixelGreen;
-  static const Color placeHolderTextColor = _solidWhite;
+  static Color get placeHolderColor => _pixelGreen;
+  static Color get placeHolderTextColor => _solidWhite;
 
-  static const Color commentTextColor = _solidBlack;
-  static const Color commentInfoTextColor = _mediumGray;
-  static const Color commentFieldColor = _lightWarmGray;
-  static const Color commentFieldHintColor = _mediumGray;
+  static Color get commentTextColor => _solidBlack;
+  static Color get commentInfoTextColor => _mediumGray;
+  static Color get commentFieldColor => _lightWarmGray;
+  static Color get commentFieldHintColor => _mediumGray;
 }
