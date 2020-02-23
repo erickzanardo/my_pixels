854a1abe9ca4914e7e5e6e150109c246f775075e
Tyemy Kuga <tyemy.kuga@dextra-sw.com>
2020-02-23 17:34:50 -0300

    Header profile

--- lib/pages/my_pixels/my_pixels.dart
+++ lib/pages/my_pixels/my_pixels.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/widgets.dart';
+import '../../widgets/button.dart';
 import '../../widgets/user_photo.dart';
 import '../../widgets/scaffold.dart';
 import './widgets/user_stats.dart';
 
 class MyPixels extends StatelessWidget {
+  final bool myself;
+  final bool following;
+
+  MyPixels({this.myself = true, this.following = false});
+
+  String _profileButtonLabel() {
+    if (myself) {
+      return "Edit";
+    }
+    return following ? "Unfollow" : "Follow";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               children: [
@@ -26,7 +40,18 @@ class MyPixels extends StatelessWidget {
                 ),
               ],
             ),
-            Text('My Pixels'),
+            Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: Text('Tyemy Kuga',
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            Padding(
+                padding: EdgeInsets.only(left: 15),
+                child: Text('Biografia da Tyemy Kuga')),
+            Center(
+                child: Button(
+              label: _profileButtonLabel(),
+              onPressed: () {},
+            ))
           ],
         ),
       ),
