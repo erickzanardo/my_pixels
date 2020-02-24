4d1c1912bc0938f4937f2191415d70fabf4427d7
Rafael <rafael.sermenho@gmail.com>
2020-02-24 11:26:14 -0300

    Fix merge conflicts

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -22,10 +22,10 @@ class Home extends StatelessWidget {
                   onPressed: (){
                     Navigator.of(context).pushNamed('/select_pixel');
                   }
-              )
+              ),
             ],
           ),
-        ]),
+        ),
     ));
   }
 }
